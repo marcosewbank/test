0857f46c8e9e9c3bb02a1cce875cd863a27cc8d2
raissa campos <raissa.campos26@gmail.com>
2023-05-24 15:46:16 +0100

    feat: top banner mobile

---
+++ components/new-home/NewHome.tsx
@@ -0,0 +1,14 @@
+import TopBanner, { TopBannerProps } from './TopBanner.tsx'
+
+export interface Props {
+  topBanner: TopBannerProps
+}
+function NewHome(props: Props) {
+  return (
+  <>
+    <TopBanner {...props.topBanner} />
+  </>
+  )
+}
+
+export default NewHome;
---
+++ components/new-home/TopBanner.tsx
@@ -0,0 +1,54 @@
+import { Picture, Source } from "deco-sites/std/components/Picture.tsx";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+
+export interface TopBannerProps {
+  /** @description RegExp to enable this banner on the current URL. Use /feminino/* to display this banner on feminino category  */
+  matcher: string;
+  /** @description text to be rendered on top of the image */
+  title?: string;
+  /** @description text to be rendered on top of the image */
+  subtitle?: string;
+  image: {
+    /** @description Image for big screens */
+    desktop: LiveImage;
+    /** @description Image for small screens */
+    mobile: LiveImage;
+    /** @description image alt text */
+    alt?: string;
+  };
+}
+
+function TopBanner(props:  TopBannerProps ) {
+  const { title, subtitle, image } = props;
+  return (
+    <div class="grid grid-cols-1 grid-rows-1">
+      <Picture preload class="col-start-1 col-span-1 row-start-1 row-span-1">
+        <Source
+          src={image.mobile}
+          width={360}
+          height={845}
+          media="(max-width: 767px)"
+        />
+        <Source
+          src={image.desktop}
+          width={1440}
+          height={200}
+          media="(min-width: 767px)"
+        />
+        <img class="w-full" src={image.desktop} alt={image.alt ?? title} />
+      </Picture>
+      <div class="container flex flex-col justify-evenly max-h-[70%] max-w-[80%] items-start col-start-1 col-span-1 row-start-1 row-span-1 w-full">
+        <h1>
+          <span class="text-white text-4xl">{title}</span>
+        </h1>
+        <div class="flex justify-end">
+        <h2 class="max-w-[70%]">
+          <span class="text-white text-sm">{subtitle}</span>
+        </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TopBanner;
